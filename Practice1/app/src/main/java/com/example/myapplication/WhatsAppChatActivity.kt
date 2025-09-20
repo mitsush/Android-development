6609c3959038537abf2dc02eb31247d55349eada
mitsush <mitsush.g@gmail.com>
2025-09-20 14:18:32 +0500

    Practice 1

---
+++ Practice1/app/src/main/java/com/example/myapplication/WhatsAppChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+
+class WhatsAppChatActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_whatsapp_chat)
+    }
+}
